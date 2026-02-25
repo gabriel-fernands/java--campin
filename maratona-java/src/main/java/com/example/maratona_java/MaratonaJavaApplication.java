e72c07eee4ac3217ae2e7c9148fd59600dc64e5c
Gabriel Fernandes <mendesgabrilfernandes@gmail.com>
2026-02-25 10:11:46 -0300

    Initial commit

---
+++ maratona-java/src/main/java/com/example/maratona_java/MaratonaJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.maratona_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaratonaJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaratonaJavaApplication.class, args);
+	}
+
+}
---
+++ maratona-java/src/test/java/com/example/maratona_java/MaratonaJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.maratona_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaratonaJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
