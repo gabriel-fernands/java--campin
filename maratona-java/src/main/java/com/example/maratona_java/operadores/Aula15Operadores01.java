a313ee6d8773a0c82bae9fb13ca124c376826420
Gabriel Fernandes <mendesgabrilfernandes@gmail.com>
2026-02-25 10:26:38 -0300

    feat: nova funcionalidade

---
+++ maratona-java/src/main/java/com/example/maratona_java/operadores/Aula15Operadores01.java
@@ -0,0 +1,10 @@
+package com.example.maratona_java.operadores;
+
+public class Aula15Operadores01 {
+    public static void main(String[] args) {
+        int num01 = 10;
+        int num02 = 20;
+
+        System.out.println("Valor: " + (num01+num02));
+    }
+}
