c4607794bb45dce36f8dd587ba43f306dd396c5c
Gabriel Fernandes <mendesgabrilfernandes@gmail.com>
2026-02-26 22:37:12 -0300

    operadores de atribuição

---
+++ maratona-java/src/main/java/com/example/maratona_java/operadores/Aula18Operadores04.java
@@ -0,0 +1,16 @@
+package com.example.maratona_java.operadores;
+
+public class Aula18Operadores04 {
+    public static void main(String[] args) {
+
+        double bonus = 1800;
+        //bonus = bonus + 1000;
+        bonus += 1000;
+        bonus -= 1000;
+        bonus *= 1000;
+        bonus /= 1000;
+        System.out.println(bonus);
+    }
+
+
+}
