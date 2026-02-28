99384963f0465461ac841121e62eef29a471b67c
Gabriel Fernandes <mendesgabrilfernandes@gmail.com>
2026-02-28 15:44:29 -0300

    swite case

---
+++ maratona-java/src/main/java/com/example/maratona_java/operadores/Aula21Operadores07.java
@@ -0,0 +1,41 @@
+package com.example.maratona_java.operadores;
+
+public class Aula21Operadores07 {
+    public static void main(String[] args) {
+
+        byte dia = 5;
+
+        switch (dia) {
+
+            case 1:
+                System.out.println("Domingo");
+                break;
+            case 2:
+                System.out.println("Segunda");
+                break;
+            case 3:
+                System.out.println("Terça");
+                break;
+            case 4:
+                System.out.println("Quarta");
+                break;
+            case 5:
+                System.out.println("Quinta");
+                break;
+            case 6:
+                System.out.println("Sexta");
+                break;
+            case 7:
+                System.out.println("Sabado");
+                break;
+            default:
+                System.out.println("invalido");
+                break;
+
+
+        }
+
+    }
+
+
+}
