82c4621689b8c4c73ab0e491653d9bb84c713e5e
Gabriel Fernandes <mendesgabrilfernandes@gmail.com>
2026-02-28 14:34:36 -0300

    Estrutura condicional

---
+++ maratona-java/src/main/java/com/example/maratona_java/operadores/Aula19Operadores05.java
@@ -0,0 +1,18 @@
+package com.example.maratona_java.operadores;
+
+public class Aula19Operadores05 {
+    public static void main(String[] args) {
+
+        int idade = 18;
+
+        boolean isAutorizadoCompraBebida = idade >= 18;
+
+        if (isAutorizadoCompraBebida) {
+            System.out.println("Autorizado a comprar bebida alcolica");
+        }else {
+            System.out.println(" Não autorizado a comprar bebida alcolica");
+        }
+        }
+
+
+    }
