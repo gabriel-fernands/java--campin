5b07ec26e47ba48b3a3c0f549e5e1773576996ff
Gabriel Fernandes <mendesgabrilfernandes@gmail.com>
2026-03-01 00:07:36 -0300

    for while

---
+++ maratona-java/src/main/java/com/example/maratona_java/operadores/Aula22Operadores08.java
@@ -0,0 +1,18 @@
+package com.example.maratona_java.operadores;
+
+public class Aula22Operadores08 {
+    public static void main(String[] args) {
+int count = 0;
+
+while (count < 10){
+    System.out.println(++count);
+
+}
+        for (int i = 0; i < 10; i++) {
+            System.out.println("For " +i);
+        }
+
+    }
+
+
+}
